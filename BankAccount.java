813574db79356159e864eb6e388c47488c831653
Logan <lruzzier20@stuy.edu>
2020-10-28 21:38:49 -0400

    added constructor

--- BankAccount.java
+++ BankAccount.java
@@ -2,4 +2,10 @@ public class BankAccount{
   private double balance;
   private int accountID;
   private String password;
+
+  public BankAccount(int a, String p){
+    balance=0;
+    accountID=a;
+    password=p;
+  }
 }
