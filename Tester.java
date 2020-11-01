c8dd5ac60992b46e30334779ef25cdb0d5b18746
Logan <lruzzier20@stuy.edu>
2020-11-01 18:33:42 -0500

    added classwork

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args){
     BankAccount x = new BankAccount(1, "test");
+    BankAccount y = new BankAccount(2, "test2");
     System.out.println(x.getAccountID());
     System.out.println(x.getBalance());
     x.setPassword("cheese");
@@ -10,5 +11,9 @@ public class Tester{
     System.out.println(x.getBalance());
     x.withdraw(1000);
     System.out.println(x.toString());
+    System.out.println(x);
+    System.out.println(x.transferTo(y, 100, "cheese"));
+    System.out.println(x);
+    System.out.println(y);
   }
 }
